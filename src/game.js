4555cafb82e85f44e7ff9394d8f47b3ea429ba28
Heikki Partanen <heikki.partanen@f-secure.com>
2017-03-26 13:39:58 +0300

    Update player stats when saving game

--- src/game.js
+++ src/game.js
@@ -1,27 +1,46 @@
 'use strict';
 
 const common = require('./common');
+const db = require('./lib/db');
 const errors = require('./lib/errors');
 const models = require('./lib/models');
 const addCORS = common.addCORS;
 
 exports.create = function (event, context, callback) {
-  let model;
+  let game;
   return common.parseBody(event.body)
     .then(function (params) {
       if (!params) {
         throw new errors.RequiredPropertyMissingError("Missing property: series");
       }
-      model = new models.Game(params);
-      return model.populate(); // Populate to verify all keys
+      game = new models.Game(params);
+      return game.populate(); // Populate to verify all keys
     })
     .then(function () {
-      return model.save();
+      return game.save();
+    })
+    .then(function () {
+      // Update player stats
+      const winners = game.goalsAway > game.goalsHome ? game.playersAway : game.playersHome;
+      const statPromises = game.playersAway.concat(game.playersHome).map(function (p) {
+        const params = {
+          "statNumberOfGames": {
+            "step": 1,
+          },
+        };
+        if (winners.indexOf(p) !== -1) {
+          params.statNumberOfWins = {
+            "step": 1,
+          };
+        }
+        return db.incrementFields(p, params);
+      });
+      return Promise.all(statPromises);
     })
     .then(function () {
       return callback(null, addCORS(event, {
         statusCode: 200,
-        body: JSON.stringify({data: model.serialize()}),
+        body: JSON.stringify({data: game.serialize()}),
       }));
     })
     .catch(function (err) {
